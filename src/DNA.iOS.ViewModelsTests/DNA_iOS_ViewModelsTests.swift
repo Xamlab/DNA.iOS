2d52602f4f89b05734b4024e23721fa65cabe5bf
Gagik Kyurkchyan <gagik.kyurkchyan@gmail.com>
2019-07-19 23:47:07 +0300

    Added core and view models projects.

---
+++ src/DNA.iOS.ViewModelsTests/DNA_iOS_ViewModelsTests.swift
@@ -0,0 +1,34 @@
+//
+//  DNA_iOS_ViewModelsTests.swift
+//  DNA.iOS.ViewModelsTests
+//
+//  Created by Gagik Kyurkchyan on 7/19/19.
+//  Copyright © 2019 Xamlab. All rights reserved.
+//
+
+import XCTest
+@testable import DNA_iOS_ViewModels
+
+class DNA_iOS_ViewModelsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
